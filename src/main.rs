b18ac66edd12695fd012a0a124705715f33ce83b
SATHVIK SVS <svssathvik77@gmail.com>
2025-02-04 14:12:33 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use axum::{response::IntoResponse, routing::get, serve, Router};
+
+async fn home() -> impl IntoResponse {
+    "Welcome to db metrics assignment"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(home));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    serve(listener, app).await.unwrap();
+}
